b86a9b1a662bd045fbc5af824d16cad44e609c6e
soheila <soheilalotfi@ymail.com>
2019-11-20 14:02:37 -0500

    add bootbox for all the card

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,14 @@
 
 $(document).on("click", "#learn-more-project1", handleproject1);
+$(document).on("click", "#learn-more-project2", handleproject2);
+$(document).on("click", "#learn-more-project3", handleproject3);
 
 function handleproject1() {
     var modalText = $("<div >").append(
-        $("<h4>").text("Notes For Article: "),
+        $("<image>").attr("src", "../images/back.png"),
+        $("<h4>").text("project name"),
+        $("<p>").text("project description"),
+        $("<a>").attr("href", "#").text("view website"),
         $("<hr>")
     );
     // Adding the formatted HTML to the note modal
@@ -13,4 +18,31 @@ function handleproject1() {
     });
 }
 
-
+function handleproject2() {
+    var modalText = $("<div >").append(
+        $("<image>").attr("src", "../images/back.png"),
+        $("<h4>").text("project name"),
+        $("<p>").text("project description"),
+        $("<a>").attr("href", "#").text("view website"),
+        $("<hr>")
+    );
+    // Adding the formatted HTML to the note modal
+    bootbox.dialog({
+        message: modalText,
+        closeButton: true
+    });
+}
+function handleproject3() {
+    var modalText = $("<div >").append(
+        $("<image>").attr("src", "../images/back.png"),
+        $("<h4>").text("project name"),
+        $("<p>").text("project description"),
+        $("<a>").attr("href", "#").text("view website"),
+        $("<hr>")
+    );
+    // Adding the formatted HTML to the note modal
+    bootbox.dialog({
+        message: modalText,
+        closeButton: true
+    });
+}
